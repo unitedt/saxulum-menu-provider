2339237454b124c5a711965d1d061b4b48d98348
unitedt <denis@chuprunov.name>
2017-01-01 09:46:39 +0300

    Update SaxulumMenuProvider.php

--- src/Provider/SaxulumMenuProvider.php
+++ src/Provider/SaxulumMenuProvider.php
@@ -10,12 +10,12 @@ class SaxulumMenuProvider
 {
     public function register(Container $container)
     {
-        $container['knp_menu.route.voter'] = $container->share(function (\Pimple $container) {
+        $container['knp_menu.route.voter'] = function (Container $container) {
             $voter = new RouteVoter();
             $voter->setRequest($container['request_stack']->getCurrentRequest());
 
             return $voter;
-        });
+        };
 
         $container['knp_menu.matcher.configure'] = $container->protect(function (Matcher $matcher) use ($container) {
             $matcher->addVoter($container['knp_menu.route.voter']);
